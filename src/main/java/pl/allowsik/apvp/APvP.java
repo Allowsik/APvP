f85e17d8c7ef6deeb1af7c2729160b97f1a9d1d1
Allowsik <allowsofficial@gmail.com>
2023-08-17 13:29:14 +0200

    First commit

---
+++ src/main/java/pl/allowsik/apvp/APvP.java
@@ -0,0 +1,17 @@
+package pl.allowsik.apvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class APvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
